307eee8adf4f9266ed91757bcf7b6c14438945a5
yasin kaya <74669908+yasinanill@users.noreply.github.com>
2023-01-13 23:06:09 +0300

    add-core-layer

---
+++ NLayerApp/NLayer.Core/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+        //Classlarımız icin ortak propertylerimizi tanımladık
+        public int ID { get; set; }
+        public DateTime CreatedTime   { get; set; }
+        public DateTime UpdatedTime   { get; set; }
+
+    }
+}
