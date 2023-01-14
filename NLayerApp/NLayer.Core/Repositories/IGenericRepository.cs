f193c77af509a8cec2dfc9be5125a1429ad77649
yasin kaya <74669908+yasinanill@users.noreply.github.com>
2023-01-14 23:43:16 +0300

    Repository-layer

---
+++ NLayerApp/NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public  interface IGenericRepository<T> where T : class
+    {
+
+        Task<T>   GetByIdAsync(int id);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);  // list kullanmamamizin sebebi direk olarak ver' tabani baglantisi yapmamasi ve performans acisindan faydasi
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);  
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync (T entity);
+        void Update (T entity);
+        void Delete (T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities );
+        Task RemoveRangeAsync(IEnumerable<T> entities );
+
+    }
+}
